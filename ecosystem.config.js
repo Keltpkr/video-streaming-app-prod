3267b38e6f23c4b5ac3297c0b73c4b7741a6f132
Gérald Babin <keltpkr@gmail.com>
2024-12-19 01:47:53 +0100

    fix:l'application ne démarre pas en production avec pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,10 +5,16 @@ module.exports = {
         script: "index.js",
         env_production: {
           NODE_ENV: "production",
-          PORT: 8080,
+          PORT: 8000,
+          IP: "0.0.0.0",
           VIDEO_DIR: "/mnt/usbdrive/Films"
+        },
+        env_development: {
+          NODE_ENV: "development",
+          DEV_IP: "0.0.0.0",
+          DEV_PORT: 8001,
+          DEV_VIDEO_DIR: "//192.168.1.46/USBDrive/Films"
         }
       }
     ]
-  };
-  
+};
